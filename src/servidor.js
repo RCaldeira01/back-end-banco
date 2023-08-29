59d4703dae2b1671e163ef7fb102711f8aca27ca
RCaldeira01 <rafael.pdscaldeira@gmail.com>
2023-08-29 15:17:07 -0300

    adicionando o vercel

--- src/servidor.js
+++ src/servidor.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const rotas = require('./rotas');
+const cors = require('cors');
 
 const app = express();
-
+app.use(
+  cors({
+    origin: '*', // Substitua pelo URL do site específico
+  }),
+);
 app.use(express.json());
 app.use(rotas);
 
